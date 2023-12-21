f465c4f21a4dd65e7d9072ea5213ae3a7ede33db
AliDzII <aruukemyrzalieva@gmail.com>
2023-12-21 18:43:37 +0600

    add:maps

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,28 @@ var swiper = new Swiper(".team__swiper", {
     },
     loop: true
 });
+
+let burger = document.querySelector('.header__burger')
+let burgerMenu = document.querySelector('.header__burgerMenu')
+
+burger.addEventListener('click',() => {
+    burger.classList.toggle('active')
+    burgerMenu.classList.toggle('active')
+})
+
+let openOverlay = document.querySelector('.header__register')
+let overlay = document.querySelector('.overlay')
+let closeOverlay = document.querySelector('.close')
+openOverlay.addEventListener('click', () => {
+        overlay.style.display = 'flex'
+})
+closeOverlay.addEventListener('click', () => {
+    overlay.style.display = 'none'
+})
+overlay.addEventListener('click', (e) => {
+    if (e.target.className === 'overlay')
+    overlay.style.display = 'none'
+})
+
+
+
